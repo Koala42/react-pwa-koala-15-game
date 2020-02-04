33b8f709a1a5f308363064234b9456f128e9f2f6
Filip Krayem <filipkrayem@gmail.com>
2020-02-04 15:27:59 +0100

    delete social details

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,8 @@
 import React, { Fragment } from "react";
 import { KeyBoardManagar } from "../hoc";
 
-import { Instruction, Header, Game, Details } from "../components";
-import { Container, Wave, GameFactoryConsumer } from "../elements";
-
-import Waves from "../assets/img/waves.gif";
+import { Instruction, Header, Game } from "../components";
+import { Container, GameFactoryConsumer } from "../elements";
 
 const Home = ({ eventType }) => {
   return (
@@ -18,17 +16,6 @@ const Home = ({ eventType }) => {
               <Game eventType={eventType} {...values} {...methods} />
               <br />
               <Instruction />
-              <div>
-                <Wave className="waves" src={Waves} alt="" />
-              </div>
-              <br />
-              <Details
-                name={"Shubham Singh"}
-                githubURL={"https://github.com/imshubhamsingh"}
-                projectURL={"https://github.com/imshubhamsingh/15-puzzle"}
-                linkedinURL={"https://linkedin.com/in/imshubhamsingh97/"}
-                twitterURL={"https://twitter.com/imshubhamsingh_"}
-              />
             </Fragment>
           );
         }}
